3c8916e040c7ae4d2463f328be92241425dc37fd
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-04-15 14:27:27 +0200

    Wind dragon player interaction

--- scripts/wind_dragon.gd
+++ scripts/wind_dragon.gd
@@ -1,28 +1,24 @@
 extends Node3D
 
-@export var force_strength: float = 300.0
-# @export var impulse_frequency: float = 0.3
-@export var max_distance: float = 10.0
+@export var force_strength: float = 2000.0
+@export var max_distance: float = 15.0
 
 var aoe: Area3D
-
+var cylinder_shape: CylinderShape3D # NOTE: Dynamic shape for testing or future effects
 func _ready():
+    cylinder_shape = $Aoe/CollisionShape3D.shape
     aoe = get_node("Aoe")
-    var cylinder_shape: CylinderShape3D = $Aoe/CollisionShape3D.shape
-    cylinder_shape.height = max_distance
-    $Aoe/CollisionShape3D.position.y = max_distance / 2
 
 func _physics_process(delta):
-
+    var cylinder_shape: CylinderShape3D = $Aoe/CollisionShape3D.shape
+    cylinder_shape.height = max_distance
+    $Aoe/CollisionShape3D.position.y = max_distance / 2 + 1a.0 # 2 is margin
     for body in aoe.get_overlapping_bodies():
         if body is Summoner:
-            var force_direction: Vector3  = Vector3.UP.rotated(Vector3.FORWARD, self.rotation.z).normalized()
-            
-            # TODO this logic is only for close to UP levitating: example: if dot_product(UP, force_direction) > 0.9
-            if body.position.y - position.y > max_distance:
-                body.position.y = position.y + max_distance
-                var animation_player: AnimationPlayer = body.get_node("AnimationPlayer")
-                animation_player.set_current_animation("levitate")
-            else:
-                body.force_accumulator += force_direction*force_strength
-                
+            var force_direction: Vector3  = Vector3.UP.rotated(Vector3.FORWARD, -self.rotation.z).normalized()
+            print(force_direction)
+            # TODO: this logic is only for close to UP levitating: example: if dot_product(UP, force_direction) > 0.9
+            var attenuation: float = 1.0-clamp((body.position.distance_to(position) / max_distance), 0.0, 1.0)
+            body.force_accumulator += force_direction*force_strength*attenuation
+            var animation_player: AnimationPlayer = body.get_node("AnimationPlayer")
+            animation_player.set_current_animation("levitate")
