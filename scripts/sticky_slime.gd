4c8486d74e75adf93114d06970649b4ca9a0c502
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2024-04-15 18:30:31 +0200

    Fix dragon and slime colission layers

--- scripts/sticky_slime.gd
+++ scripts/sticky_slime.gd
@@ -9,7 +9,6 @@ func _stick(body: Node3D):
     if body is Summoner:
         body.stuck = true
     elif body is WindDragon:
-        print("Wind Dragon")
         body.freeze = true
 
 func _unstick(body: Node3D):
